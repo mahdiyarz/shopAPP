d4cf184533b6365ffb7c57179f49cedb38044990
mahdiyarz <m.arbabzi@gmail.com>
2021-07-21 06:14:21 -0700

    create mainScreen.dart

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  static const routeName = '/main-screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Main Screen'),
+      ),
+    );
+  }
+}
