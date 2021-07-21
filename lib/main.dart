83be9638e043835cf12781c635c1ef8ece80c14e
mahdiyarz <m.arbabzi@gmail.com>
2021-07-21 07:50:43 -0700

    set new theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.cyan,
+        accentColor: Colors.grey,
       ),
       initialRoute: '/',
       routes: {
