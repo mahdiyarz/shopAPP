2b933c528dd7cd56e5272b77f2af59123ad321d5
mahdiyarz <m.arbabzi@gmail.com>
2021-07-23 04:45:02 -0700

    config our productProvider

---
+++ lib/providers/product_provider.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class ProductProvider with ChangeNotifier {
+  List<Product> _items = [
+    Product(
+      id: 'p1',
+      title: 'Red Shirt',
+      description: 'A red shirt - it is pretty red!',
+      price: 29.99,
+      imageUrl:
+          'https://rukminim1.flixcart.com/image/714/857/juh9ksw0/shirt/g/f/j/xl-red-casual-shirt-qlonz-store-original-imaffhcbrb4kgsuv.jpeg?q=50',
+    ),
+    Product(
+      id: 'p2',
+      title: 'Trousers',
+      description: 'A nice pair of trousers.',
+      price: 59.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    ),
+    Product(
+      id: 'p3',
+      title: 'Yellow Scarf',
+      description: 'Warm and cozy - exactly what you need for the winter.',
+      price: 19.99,
+      imageUrl: 'https://m.media-amazon.com/images/I/417Kbn0NO8L.jpg',
+    ),
+    Product(
+      id: 'p4',
+      title: 'A Pan',
+      description: 'Prepare any meal you want.',
+      price: 49.99,
+      imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    ),
+  ];
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  void addProduct() {
+    notifyListeners();
+  }
+}
