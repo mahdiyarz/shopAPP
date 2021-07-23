d8d00c7c3c245cb3becbb8e30bc68ac95d16b4e2
mahdiyarz <m.arbabzi@gmail.com>
2021-07-23 04:47:47 -0700

    add productProvider to mainPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import './screens/main_screen.dart';
 import './screens/product_details_screen.dart';
+import './providers/product_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,18 +13,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-        accentColor: Colors.grey,
-        fontFamily: 'Roboto',
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+          accentColor: Colors.grey,
+          fontFamily: 'Roboto',
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (ctx) => MainScreen(),
+          ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => MainScreen(),
-        ProductDetailsScreen.routeName: (ctx) => ProductDetailsScreen(),
-      },
     );
   }
 }
