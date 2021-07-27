84bd5dc301377266680ef863fd36b9efa542240f
mahdiyarz <m.arbabzi@gmail.com>
2021-07-27 07:22:32 -0700

    create cart provider and set it up

---
+++ lib/providers/cart.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/foundation.dart';
+
+class CartItem {
+  final String? id;
+  final String? title;
+  final int? quantity;
+  final double? price;
+
+  CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price,
+  });
+}
+
+class Cart with ChangeNotifier {
+  Map<String, CartItem> _items = {};
+
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingItems) => CartItem(
+              id: existingItems.id,
+              title: existingItems.title,
+              quantity: existingItems.quantity! + 1,
+              price: existingItems.price));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+              id: DateTime.now().toString(),
+              title: title,
+              quantity: 1,
+              price: price));
+    }
+  }
+}
