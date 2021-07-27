feed187a22704c7f9fc4332e991997b06739adfb
mahdiyarz <m.arbabzi@gmail.com>
2021-07-27 07:25:43 -0700

    connect cart provider to main page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import './screens/main_screen.dart';
 import './screens/product_details_screen.dart';
 import './providers/product_provider.dart';
+import './providers/cart.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,8 +14,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => ProductProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => ProductProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
