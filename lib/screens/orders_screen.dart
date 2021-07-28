b8338db2ba6fb13bfdbf73daf56b071f1d36bbfc
mahdiyarz <m.arbabzi@gmail.com>
2021-07-28 04:10:32 -0700

    create order screen

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class OrdersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      body: Container(),
+    );
+  }
+}
