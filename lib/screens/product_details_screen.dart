6f11ad6ce610ea27d9b939cfb1f372ee7b691eee
mahdiyarz <m.arbabzi@gmail.com>
2021-07-28 03:27:24 -0700

    style first look of product details page

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -18,7 +18,34 @@ class ProductDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(loadedProduct.title as String),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl as String,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              loadedProduct.description as String,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
