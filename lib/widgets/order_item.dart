6642906434829c8a96b432830cc94e82376e8a45
mahdiyarz <m.arbabzi@gmail.com>
2021-07-29 23:30:20 +0430

    get style on order item widget

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../providers/order.dart' as orderModel;
 
 class OrderItem extends StatelessWidget {
+  final orderModel.OrderItem ordersData;
+  OrderItem(this.ordersData);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('\$${ordersData.amount}'),
+            subtitle: Text(DateFormat('dd/MM/yyyy - hh:mm')
+                .format(ordersData.dateTime as DateTime)),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
