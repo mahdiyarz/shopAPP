a984d3ab34079b7ba4d04e92e9e9a44bc5e96463
mahdiyarz <m.arbabzi@gmail.com>
2021-07-29 23:35:56 +0430

    connect order item widget with order screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/order.dart' show Order;
+import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ordersData = Provider.of<Order>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: ordersData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(
+          ordersData.orders[i],
+        ),
+      ),
     );
   }
 }
