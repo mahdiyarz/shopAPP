58c41e6f0ff049e93387a4654420ba1916425854
mahdiyarz <m.arbabzi@gmail.com>
2021-08-01 15:35:10 +0430

    create edite screen

---
+++ lib/screens/edite_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EditeScreen extends StatefulWidget {
+  @override
+  _EditeScreenState createState() => _EditeScreenState();
+}
+
+class _EditeScreenState extends State<EditeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
