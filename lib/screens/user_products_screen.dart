4a99f3b9f9306b1caa0e41140f993962f051ee55
mahdiyarz <m.arbabzi@gmail.com>
2021-08-01 14:38:39 +0430

    connect user product widget on user product screen

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/product_provider.dart';
+import '../widgets/user_products.dart';
 
 class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-product';
   @override
   Widget build(BuildContext context) {
+    final productData = Provider.of<ProductProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('User Products'),
@@ -14,7 +19,19 @@ class UserProductsScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Container(),
+      body: ListView.builder(
+          itemCount: productData.items.length,
+          itemBuilder: (_, i) {
+            return Column(
+              children: [
+                UserProducts(
+                  productData.items[i].title as String,
+                  productData.items[i].imageUrl as String,
+                ),
+                Divider(),
+              ],
+            );
+          }),
     );
   }
 }
