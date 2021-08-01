dc5f090eb824896112b051d03156cb0c93f02ea7
mahdiyarz <m.arbabzi@gmail.com>
2021-08-01 19:37:18 +0430

    create title & price text field on edit product screen

--- lib/screens/edite_screen.dart
+++ lib/screens/edite_screen.dart
@@ -7,13 +7,35 @@ class EditeScreen extends StatefulWidget {
 }
 
 class _EditeScreenState extends State<EditeScreen> {
+  final _priceFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Products'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
