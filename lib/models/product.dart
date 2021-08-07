8a5d58015f877f793a1c435868d57c0c669de631
mahdiyarz <m.arbabzi@gmail.com>
2021-08-07 20:32:22 +0430

    create fav logic for database updating

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String? id;
@@ -25,8 +28,28 @@ class Product with ChangeNotifier {
     @required this.isScarf,
   });
 
-  void toggleFavoriteStatus() {
+  void _favoriteStatus(bool newStatus) {
+    isFavorate = newStatus;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorate;
+    final url = Uri.https(
+        'my-shop-app-5ef04-default-rtdb.asia-southeast1.firebasedatabase.app',
+        '/products/$id.json');
     isFavorate = !isFavorate;
     notifyListeners();
+    try {
+      final response = await http.patch(url,
+          body: json.encode({
+            'isFavorite': isFavorate,
+          }));
+      if (response.statusCode >= 400) {
+        _favoriteStatus(oldStatus);
+      }
+    } catch (error) {
+      _favoriteStatus(oldStatus);
+    }
   }
 }
