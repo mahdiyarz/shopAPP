00d8e7c57ad1b70f19ee34a4edfff1d6af72e9ef
mahdiyarz <m.arbabzi@gmail.com>
2021-08-07 19:59:32 +0430

    delete dismiss widget and set delete logic here with icon button

--- lib/widgets/user_products.dart
+++ lib/widgets/user_products.dart
@@ -13,65 +13,47 @@ class UserProducts extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Dismissible(
-      key: ValueKey(productId),
-      background: Container(
-        color: Colors.red.shade400,
-        child: Icon(
-          Icons.delete,
-          color: Colors.white,
-        ),
-        alignment: Alignment.centerRight,
-        padding: EdgeInsets.all(5),
+    final scaffold = ScaffoldMessenger.of(context);
+    return ListTile(
+      title: Text(title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
       ),
-      direction: DismissDirection.endToStart,
-      onDismissed: (direction) {
-        Provider.of<ProductProvider>(context, listen: false)
-            .deleteProduct(productId);
-      },
-      child: ListTile(
-        title: Text(title),
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(imageUrl),
-        ),
-        trailing: Container(
-          width: 100,
-          child: Column(
-            children: [
-              Container(
-                height: 15,
-                child: Row(
-                  children: [
-                    Icon(Icons.keyboard_arrow_left),
-                    Text(
-                      'delete',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  ],
-                ),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.edit,
+                color: Colors.blueGrey,
               ),
-              Container(
-                height: 35,
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.keyboard_arrow_right),
-                      onPressed: () {
-                        Navigator.of(context).pushNamed(
-                          EditeScreen.routeName,
-                          arguments: productId,
-                        );
-                      },
-                    ),
-                    Text(
-                      'edite',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  ],
-                ),
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  EditeScreen.routeName,
+                  arguments: productId,
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.delete,
+                color: Colors.redAccent,
               ),
-            ],
-          ),
+              onPressed: () async {
+                try {
+                  await Provider.of<ProductProvider>(context, listen: false)
+                      .deleteProduct(productId);
+                } catch (error) {
+                  scaffold.showSnackBar(SnackBar(
+                      content: Text(
+                    'Delete Faild!',
+                    textAlign: TextAlign.center,
+                  )));
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
