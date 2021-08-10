57a2070b6245c08928e721548bb58a1c910700d6
mahdiyarz <m.arbabzi@gmail.com>
2021-08-10 21:10:13 +0430

    Set style for auth widget

--- lib/widgets/auth.dart
+++ lib/widgets/auth.dart
@@ -1,4 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+enum AuthMode {
+  Login,
+  SignUp,
+}
 
 class AuthCard extends StatefulWidget {
   @override
@@ -6,10 +12,84 @@ class AuthCard extends StatefulWidget {
 }
 
 class _AuthCardState extends State<AuthCard> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  AuthMode _authMode = AuthMode.Login;
+  Map<String, String> _authData = {
+    'email': '',
+    'password': '',
+  };
+  var _isLoading = false;
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    final deviceSize = MediaQuery.of(context).size;
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      elevation: 8.0,
+      child: Container(
+        height: _authMode == AuthMode.SignUp ? 320 : 260,
+        // constraints: BoxConstraints(minHeight: _authMode == AuthMode.SignUp ? 320 : 260),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'e-Mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (_) {},
+                  onSaved: (_) {},
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  controller: _passwordController,
+                  validator: (_) {},
+                  onSaved: (_) {},
+                ),
+                if (_authMode == AuthMode.SignUp)
+                  TextFormField(
+                    enabled: _authMode == AuthMode.SignUp,
+                    decoration: InputDecoration(labelText: 'Confirm Password'),
+                    obscureText: true,
+                    validator: (_) {},
+                  ),
+                SizedBox(
+                  height: 20,
+                ),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  ElevatedButton(
+                    onPressed: () {},
+                    child:
+                        Text(_authMode == AuthMode.Login ? 'Login' : 'SignUp'),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                      primary: Theme.of(context).primaryColor,
+                      onPrimary: Colors.white,
+                    ),
+                  ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(_authMode == AuthMode.Login
+                      ? 'SignUp here'
+                      : 'Login to Shop'),
+                  style: TextButton.styleFrom(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
