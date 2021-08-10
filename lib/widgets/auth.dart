4f84b16ba6a6d80e9640f17e64b0d1efafa23e2e
mahdiyarz <m.arbabzi@gmail.com>
2021-08-10 19:40:18 +0430

    Create auth widget

---
+++ lib/widgets/auth.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthCard extends StatefulWidget {
+  @override
+  _AuthCardState createState() => _AuthCardState();
+}
+
+class _AuthCardState extends State<AuthCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
