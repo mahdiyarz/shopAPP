a60636d56339559875f92c9543d1f5847979f32c
mahdiyarz <m.arbabzi@gmail.com>
2021-08-11 14:49:49 +0430

    Create handling error massage on auth screen

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String? _token;
   DateTime? _expiryDate;
@@ -12,13 +14,20 @@ class Auth with ChangeNotifier {
       String email, String password, String urlSegment) async {
     final url = Uri.parse(
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDq_puXGG8IniHrx48K-t3Cosia19QjzQ4');
-    final response = await http.post(url,
-        body: json.encode({
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        }));
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }));
+      final responsData = json.decode(response.body);
+      if (responsData['error'] != null) {
+        throw HttpException(responsData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> singup(String email, String password) async {
--- lib/widgets/auth.dart
+++ lib/widgets/auth.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/auth_provider.dart';
+import '../models/http_exception.dart';
 
 enum AuthMode {
   Login,
@@ -23,6 +24,23 @@ class _AuthCardState extends State<AuthCard> {
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
+  void _showDialogError(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(ctx).pop();
+              },
+              child: Text('Okay'))
+        ],
+      ),
+    );
+  }
+
   Future<void> _submit() async {
     if (!_formKey.currentState!.validate()) {
       return;
@@ -31,18 +49,38 @@ class _AuthCardState extends State<AuthCard> {
     setState(() {
       _isLoading = true;
     });
-    if (_authMode == AuthMode.Login) {
-      // Log user in
-      await Provider.of<Auth>(context, listen: false).login(
-        _authData['email'] as String,
-        _authData['password'] as String,
-      );
-    } else {
-      // sign user up
-      await Provider.of<Auth>(context, listen: false).singup(
-        _authData['email'] as String,
-        _authData['password'] as String,
-      );
+    try {
+      if (_authMode == AuthMode.Login) {
+        // Log user in
+        await Provider.of<Auth>(context, listen: false).login(
+          _authData['email'] as String,
+          _authData['password'] as String,
+        );
+      } else {
+        // sign user up
+        await Provider.of<Auth>(context, listen: false).singup(
+          _authData['email'] as String,
+          _authData['password'] as String,
+        );
+      }
+    } on HttpException catch (error) {
+      var errorMessage = 'Authentication failed!';
+      if (error.toString().contains('EMAIL_EXISTS')) {
+        errorMessage = 'This email address is already in use.';
+      } else if (error.toString().contains('INVALID_EMAIL')) {
+        errorMessage = 'This is not a valid email address!';
+      } else if (error.toString().contains('WEAK_PASSWORD')) {
+        errorMessage =
+            'Your password is too weak. please enter another password.';
+      } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
+        errorMessage = 'Could not find a user with that email.';
+      } else if (error.toString().contains('INVALID_PASSWORD')) {
+        errorMessage = 'Invalid password';
+      }
+      _showDialogError(errorMessage);
+    } catch (error) {
+      var errorMessage = 'Could not authenticate you. Please try againe later.';
+      _showDialogError(errorMessage);
     }
     setState(() {
       _isLoading = false;
