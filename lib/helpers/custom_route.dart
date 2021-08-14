dd7228c5aff2876dbcb011897315b6dbddd176ea
mahdiyarz <m.arbabzi@gmail.com>
2021-08-14 21:17:44 +0430

    Create custom route

---
+++ lib/helpers/custom_route.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CustomRoute extends PageTransitionsBuilder {
+  @override
+  Widget buildTransitions<T>(
+      PageRoute<T> route,
+      BuildContext context,
+      Animation<double> animation,
+      Animation<double> secondaryAnimation,
+      Widget child) {
+    if (route.settings.name == '/') {
+      return child;
+    }
+    return FadeTransition(
+      opacity: animation,
+      child: child,
+    );
+  }
+}
