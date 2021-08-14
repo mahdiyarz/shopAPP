5ff5fd212c3285de27130cd35e53ff643cf7c016
mahdiyarz <m.arbabzi@gmail.com>
2021-08-14 15:14:33 +0430

    Set animation on app bar of product details screen

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -15,16 +15,17 @@ class ProductDetailsScreen extends StatelessWidget {
       listen: false,
     ).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title as String),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              child: Hero(
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title as String),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(loadedProduct.title as String),
+              background: Hero(
                 tag: productId,
                 child: Image.network(
                   loadedProduct.imageUrl as String,
@@ -32,22 +33,30 @@ class ProductDetailsScreen extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 10),
-            Text(
-              '\$${loadedProduct.price}',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 20,
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(height: 10),
+              Text(
+                '\$${loadedProduct.price}',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
               ),
-            ),
-            SizedBox(height: 10),
-            Text(
-              loadedProduct.description as String,
-              textAlign: TextAlign.center,
-              softWrap: true,
-            ),
-          ],
-        ),
+              SizedBox(height: 10),
+              Text(
+                loadedProduct.description as String,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+              SizedBox(
+                height: 800,
+              )
+            ]),
+          ),
+        ],
       ),
     );
   }
